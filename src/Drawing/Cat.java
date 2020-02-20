9159344603065b5742807a2eea97325071d34975
Scott Oelkers <stoelkers@mymail.mines.edu>
2020-02-19 21:54:33 -0700

    Mouth width change

--- src/Drawing/Cat.java
+++ src/Drawing/Cat.java
@@ -16,7 +16,7 @@ public class Cat {
 	private static final int EYE_WIDTH = 30;
 	// pick mouth height, width is based on head dimension
 	private static final int MOUTH_HEIGHT = 10;
-	private static final int MOUTH_WIDTH = HEAD_DIMENSION/4;
+	private static final int MOUTH_WIDTH = HEAD_DIMENSION/3;
 	// mouth starts about 40% from left edge of head
 	private static final int MOUTH_X = HEAD_DIMENSION/5 * 2;
 	private static final int MOUTH_Y = HEAD_DIMENSION/5 * 3;
